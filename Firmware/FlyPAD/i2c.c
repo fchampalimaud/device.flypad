930c723645b9e11b63a33b46d613a5579ed02be9
Artur Silva <artursilva0@users.noreply.github.com>
2026-08-06 18:30:56 +0100

    Correct left-shift calcs

--- Firmware/FlyPAD/i2c.c
+++ Firmware/FlyPAD/i2c.c
@@ -12,6 +12,7 @@ extern AppRegs app_regs;
 #define _8_CLOCK_CYCLES _4_CLOCK_CYCLES; _4_CLOCK_CYCLES
 #define _16_CLOCK_CYCLES _8_CLOCK_CYCLES; _8_CLOCK_CYCLES
 #define _32_CLOCK_CYCLES _16_CLOCK_CYCLES; _16_CLOCK_CYCLES
+#define _48_CLOCK_CYCLES _32_CLOCK_CYCLES; _16_CLOCK_CYCLES 
 #define _64_CLOCK_CYCLES _32_CLOCK_CYCLES; _32_CLOCK_CYCLES
 
 void init_ios2()
@@ -136,10 +137,11 @@ void write_byte(uint8_t byte)
 		else {
             clr_sda();
 		}
+		
 		set_SCL;
-		// _32_CLOCK_CYCLES;
+		_32_CLOCK_CYCLES; // ~1us (spec 0.6us)
 		clr_SCL;
-		// _8_CLOCK_CYCLES;
+		_48_CLOCK_CYCLES; // set/clr_SDA takes around ~1.5us - add 250ns margin (spec 1.3us)
 	}
 }
 
@@ -160,7 +162,8 @@ void read_byte(uint8_t index)
     uint8_t *arr_j = &bytes_j[index];
     uint8_t *arr_a = &bytes_a[index];
     uint8_t *arr_b = &bytes_b[index];
-    for (uint8_t i = 0; i < 8; i++)
+    
+	for (uint8_t i = 0; i < 8; i++)
 	{
 		set_SCL;
         arr_d[i] = PORTD.IN;
@@ -171,7 +174,7 @@ void read_byte(uint8_t index)
         arr_a[i] = PORTA.IN;
         arr_b[i] = PORTB.IN;
 		clr_SCL;
-		_8_CLOCK_CYCLES;
+		_48_CLOCK_CYCLES;
 
 	}
 }
@@ -181,49 +184,54 @@ void read_capacitance()
 	// Start condition
     set_sda();
 	set_SCL;
-	// _32_CLOCK_CYCLES;
+	_32_CLOCK_CYCLES;
 	clr_sda();
 	clr_SCL;
+	_48_CLOCK_CYCLES;
 
     // Address AD7150 to write
-    write_byte(0x90);
-
+	write_byte(0x90);
+	
     // In theory, acknowledge bit should be read, but in practice we are just controlling the serial clock
     clr_sda();
 	set_SCL;
-	// _32_CLOCK_CYCLES;
+	_32_CLOCK_CYCLES;
 	clr_SCL;
+	_48_CLOCK_CYCLES;
 
     // Write register from where we want to start the readings
-    write_byte(0x01);
+	write_byte(0x01);
 
     // New fake acknowledge bit reading
     clr_sda();
 	set_SCL;
-	// _32_CLOCK_CYCLES;
+	_32_CLOCK_CYCLES;
 	clr_SCL;
+	_48_CLOCK_CYCLES;
 
     // New start condition, now for reading values
     set_sda();
 	set_SCL;
-	// _32_CLOCK_CYCLES;
+	_32_CLOCK_CYCLES;
 	clr_sda();
 	clr_SCL;
-
+    _48_CLOCK_CYCLES;
+	 
     // Address AD7150 to read
     write_byte(0x91);
 
     // New fake acknowledge bit reading
     clr_sda();
 	set_SCL;
-	// _32_CLOCK_CYCLES;
+	_32_CLOCK_CYCLES;
 	clr_SCL;
+	_48_CLOCK_CYCLES;
 
     // Prepare to read byte
     sda_to_in();
 
     // Read first most significant byte of capacitance for channel 0
-    read_byte(0);
+	read_byte(0);
 
     // Prepare to send acknowledge bit
     sda_to_out();
@@ -231,14 +239,15 @@ void read_capacitance()
     // Send acknowledge bit
     clr_sda();
 	set_SCL;
-	// _32_CLOCK_CYCLES;
+	_32_CLOCK_CYCLES;
 	clr_SCL;
+	_48_CLOCK_CYCLES;
 
     // Prepare to read byte
     sda_to_in();
 
     // Read first least significant byte of capacitance for channel 0
-    read_byte(8);
+	read_byte(8);
 
     // Prepare to send acknowledge bit
     sda_to_out();
@@ -246,8 +255,9 @@ void read_capacitance()
     // Send acknowledge bit
     clr_sda();
 	set_SCL;
-	// _32_CLOCK_CYCLES;
+	_32_CLOCK_CYCLES;
 	clr_SCL;
+	_48_CLOCK_CYCLES;
 
     // Prepare to read byte
     sda_to_in();
@@ -261,8 +271,9 @@ void read_capacitance()
     // Send acknowledge bit
     clr_sda();
 	set_SCL;
-	// _32_CLOCK_CYCLES;
+	_32_CLOCK_CYCLES;
 	clr_SCL;
+	_48_CLOCK_CYCLES;
 
     // Prepare to read byte
     sda_to_in();
@@ -276,21 +287,22 @@ void read_capacitance()
     // Send not-acknowledge bit
     set_sda();
 	set_SCL;
-	// _32_CLOCK_CYCLES;
+	_32_CLOCK_CYCLES;
 	clr_SCL;
     clr_sda();
+	_48_CLOCK_CYCLES;
 
     // Stop condition
 	set_SCL;
     set_sda();
-	// _32_CLOCK_CYCLES;
+	_32_CLOCK_CYCLES;
 	clr_SCL;
 	clr_sda();
 }
 
 
 /*
-    DISCLAIMER: the way the capacitances are calculated is unoptimal, but 
+    DISCLAIMER: the way the capacitances are calculated is suboptimal, but 
     calculating them in the same function makes execution time increase beyond 
     the harp core allowed timeout limit
 */
@@ -302,10 +314,10 @@ void calculate_capacitance_1()
 
     for (uint8_t i = 0; i < 16; i++)
 	{
-        app_regs.REG_CAPACITANCE_VALUES[0] |= (bytes_d[i] & 0x01) << (15 - i);
-        app_regs.REG_CAPACITANCE_VALUES[2] |= (bytes_d[i] & 0x02) << (14 - i);
-        app_regs.REG_CAPACITANCE_VALUES[4] |= (bytes_d[i] & 0x04) << (13 - i);
-        app_regs.REG_CAPACITANCE_VALUES[6] |= (bytes_d[i] & 0x08) << (12 - i);
+        app_regs.REG_CAPACITANCE_VALUES[0] |= (uint16_t)((bytes_d[i] >> 0) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[2] |= (uint16_t)((bytes_d[i] >> 1) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[4] |= (uint16_t)((bytes_d[i] >> 2) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[6] |= (uint16_t)((bytes_d[i] >> 3) & 0x01) << (15 - i);
     }
 
     // Second channel
@@ -313,10 +325,10 @@ void calculate_capacitance_1()
     uint8_t *bytes = &bytes_d[16];
     for (uint8_t i = 0; i < 16; i++)
 	{
-        reg[0] |= (bytes[i] & 0x01) << (15 - i);
-        reg[2] |= (bytes[i] & 0x02) << (14 - i);
-        reg[4] |= (bytes[i] & 0x04) << (13 - i);
-        reg[6] |= (bytes[i] & 0x08) << (12 - i);
+        reg[0] |= (uint16_t)((bytes[i] >> 0) & 0x01) << (15 - i);
+        reg[2] |= (uint16_t)((bytes[i] >> 1) & 0x01) << (15 - i);
+        reg[4] |= (uint16_t)((bytes[i] >> 2) & 0x01) << (15 - i);
+        reg[6] |= (uint16_t)((bytes[i] >> 3) & 0x01) << (15 - i);
     }
 }
 
@@ -328,10 +340,10 @@ void calculate_capacitance_2()
 
     for (uint8_t i = 0; i < 16; i++)
 	{
-        app_regs.REG_CAPACITANCE_VALUES[8] |= (bytes_d[i] & 0x10) << (11 - i);
-        app_regs.REG_CAPACITANCE_VALUES[10] |= (bytes_d[i] & 0x20) << (10 - i);
-        app_regs.REG_CAPACITANCE_VALUES[12] |= (bytes_d[i] & 0x40) << (9 - i);
-        app_regs.REG_CAPACITANCE_VALUES[14] |= (bytes_d[i] & 0x80) << (8 - i);
+        app_regs.REG_CAPACITANCE_VALUES[8] |= (uint16_t)((bytes_d[i] >> 4) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[10] |= (uint16_t)((bytes_d[i] >> 5) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[12] |= (uint16_t)((bytes_d[i] >> 6) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[14] |= (uint16_t)((bytes_d[i] >> 7) & 0x01) << (15 - i);
     }
 
     // Second channel
@@ -339,10 +351,10 @@ void calculate_capacitance_2()
     uint8_t *bytes = &bytes_d[16];
     for (uint8_t i = 0; i < 16; i++)
 	{
-        reg[8] |= (bytes[i] & 0x10) << (11 - i);
-        reg[10] |= (bytes[i] & 0x20) << (10 - i);
-        reg[12] |= (bytes[i] & 0x40) << (9 - i);
-        reg[14] |= (bytes[i] & 0x80) << (8 - i);
+        reg[8] |= (uint16_t)((bytes[i] >> 4) & 0x01) << (15 - i);
+        reg[10] |= (uint16_t)((bytes[i] >> 5) & 0x01) << (15 - i);
+        reg[12] |= (uint16_t)((bytes[i] >> 6) & 0x01) << (15 - i);
+        reg[14] |= (uint16_t)((bytes[i] >> 7) & 0x01) << (15 - i);
     }
 }
 
@@ -354,20 +366,20 @@ void calculate_capacitance_3()
 
     for (uint8_t i = 0; i < 16; i++)
 	{
-        app_regs.REG_CAPACITANCE_VALUES[16] |= (bytes_e[i] & 0x10) << (11 - i);
-        app_regs.REG_CAPACITANCE_VALUES[18] |= (bytes_e[i] & 0x20) << (10 - i);
-        app_regs.REG_CAPACITANCE_VALUES[20] |= (bytes_e[i] & 0x40) << (9 - i);
-        app_regs.REG_CAPACITANCE_VALUES[22] |= (bytes_e[i] & 0x80) << (8 - i);
+        app_regs.REG_CAPACITANCE_VALUES[16] |= (uint16_t)((bytes_e[i] >> 4) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[18] |= (uint16_t)((bytes_e[i] >> 5) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[20] |= (uint16_t)((bytes_e[i] >> 6) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[22] |= (uint16_t)((bytes_e[i] >> 7) & 0x01) << (15 - i);
     }
 
     uint16_t *reg = &app_regs.REG_CAPACITANCE_VALUES[1];
     uint8_t *bytes = &bytes_e[16];
     for (uint8_t i = 0; i < 16; i++)
 	{
-        reg[16] |= (bytes[i] & 0x10) << (11 - i);
-        reg[18] |= (bytes[i] & 0x20) << (10 - i);
-        reg[20] |= (bytes[i] & 0x40) << (9 - i);
-        reg[22] |= (bytes[i] & 0x80) << (8 - i);
+        reg[16] |= (uint16_t)((bytes[i] >> 4) & 0x01) << (15 - i);
+        reg[18] |= (uint16_t)((bytes[i] >> 5) & 0x01) << (15 - i);
+        reg[20] |= (uint16_t)((bytes[i] >> 6) & 0x01) << (15 - i);
+        reg[22] |= (uint16_t)((bytes[i] >> 7) & 0x01) << (15 - i);
     }
 }
 
@@ -379,10 +391,10 @@ void calculate_capacitance_4()
 
     for (uint8_t i = 0; i < 16; i++)
 	{
-        app_regs.REG_CAPACITANCE_VALUES[24] |= (bytes_f[i] & 0x10) << (11 - i);
-        app_regs.REG_CAPACITANCE_VALUES[26] |= (bytes_f[i] & 0x20) << (10 - i);
-        app_regs.REG_CAPACITANCE_VALUES[28] |= (bytes_f[i] & 0x40) << (9 - i);
-        app_regs.REG_CAPACITANCE_VALUES[30] |= (bytes_f[i] & 0x80) << (8 - i);
+        app_regs.REG_CAPACITANCE_VALUES[24] |= (uint16_t)((bytes_f[i] >> 4) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[26] |= (uint16_t)((bytes_f[i] >> 5) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[28] |= (uint16_t)((bytes_f[i] >> 6) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[30] |= (uint16_t)((bytes_f[i] >> 7) & 0x01) << (15 - i);
     }
 
 
@@ -391,10 +403,10 @@ void calculate_capacitance_4()
     uint8_t *bytes = &bytes_f[16];
     for (uint8_t i = 0; i < 16; i++)
 	{
-        reg[24] |= (bytes[i] & 0x10) << (11 - i);
-        reg[26] |= (bytes[i] & 0x20) << (10 - i);
-        reg[28] |= (bytes[i] & 0x40) << (9 - i);
-        reg[30] |= (bytes[i] & 0x80) << (8 - i);
+        reg[24] |= (uint16_t)((bytes[i] >> 4) & 0x01) << (15 - i);
+        reg[26] |= (uint16_t)((bytes[i] >> 5) & 0x01) << (15 - i);
+        reg[28] |= (uint16_t)((bytes[i] >> 6) & 0x01) << (15 - i);
+        reg[30] |= (uint16_t)((bytes[i] >> 7) & 0x01) << (15 - i);
     }
 }
 
@@ -406,10 +418,10 @@ void calculate_capacitance_5()
 
     for (uint8_t i = 0; i < 16; i++)
 	{
-        app_regs.REG_CAPACITANCE_VALUES[32] |= (bytes_h[i] & 0x10) << (11 - i);
-        app_regs.REG_CAPACITANCE_VALUES[34] |= (bytes_h[i] & 0x20) << (10 - i);
-        app_regs.REG_CAPACITANCE_VALUES[36] |= (bytes_h[i] & 0x40) << (9 - i);
-        app_regs.REG_CAPACITANCE_VALUES[38] |= (bytes_h[i] & 0x80) << (8 - i);
+        app_regs.REG_CAPACITANCE_VALUES[32] |= (uint16_t)((bytes_h[i] >> 4) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[34] |= (uint16_t)((bytes_h[i] >> 5) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[36] |= (uint16_t)((bytes_h[i] >> 6) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[38] |= (uint16_t)((bytes_h[i] >> 7) & 0x01) << (15 - i);
     }
 
     // Second channel
@@ -417,10 +429,10 @@ void calculate_capacitance_5()
     uint8_t *bytes = &bytes_h[16];
     for (uint8_t i = 0; i < 16; i++)
 	{
-        reg[32] |= (bytes[i] & 0x10) << (11 - i);
-        reg[34] |= (bytes[i] & 0x20) << (10 - i);
-        reg[36] |= (bytes[i] & 0x40) << (9 - i);
-        reg[38] |= (bytes[i] & 0x80) << (8 - i);
+        reg[32] |= (uint16_t)((bytes[i] >> 4) & 0x01) << (15 - i);
+        reg[34] |= (uint16_t)((bytes[i] >> 5) & 0x01) << (15 - i);
+        reg[36] |= (uint16_t)((bytes[i] >> 6) & 0x01) << (15 - i);
+        reg[38] |= (uint16_t)((bytes[i] >> 7) & 0x01) << (15 - i);
     }
 }
 
@@ -432,10 +444,10 @@ void calculate_capacitance_6()
 
     for (uint8_t i = 0; i < 16; i++)
 	{
-        app_regs.REG_CAPACITANCE_VALUES[40] |= (bytes_j[i] & 0x01) << (15 - i);
-        app_regs.REG_CAPACITANCE_VALUES[42] |= (bytes_j[i] & 0x02) << (14 - i);
-        app_regs.REG_CAPACITANCE_VALUES[44] |= (bytes_j[i] & 0x04) << (13 - i);
-        app_regs.REG_CAPACITANCE_VALUES[46] |= (bytes_j[i] & 0x08) << (12 - i);
+        app_regs.REG_CAPACITANCE_VALUES[40] |= (uint16_t)((bytes_j[i] >> 0) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[42] |= (uint16_t)((bytes_j[i] >> 1) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[44] |= (uint16_t)((bytes_j[i] >> 2) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[46] |= (uint16_t)((bytes_j[i] >> 3) & 0x01) << (15 - i);
     }
 
     // Second channel
@@ -443,10 +455,10 @@ void calculate_capacitance_6()
     uint8_t *bytes = &bytes_j[16];
     for (uint8_t i = 0; i < 16; i++)
 	{
-        reg[40] |= (bytes[i] & 0x01) << (15 - i);
-        reg[42] |= (bytes[i] & 0x02) << (14 - i);
-        reg[44] |= (bytes[i] & 0x04) << (13 - i);
-        reg[46] |= (bytes[i] & 0x08) << (12 - i);
+        reg[40] |= (uint16_t)((bytes[i] >> 0) & 0x01) << (15 - i);
+        reg[42] |= (uint16_t)((bytes[i] >> 1) & 0x01) << (15 - i);
+        reg[44] |= (uint16_t)((bytes[i] >> 2) & 0x01) << (15 - i);
+        reg[46] |= (uint16_t)((bytes[i] >> 3) & 0x01) << (15 - i);
     }
 }
 
@@ -458,10 +470,10 @@ void calculate_capacitance_7()
 
     for (uint8_t i = 0; i < 16; i++)
 	{
-        app_regs.REG_CAPACITANCE_VALUES[48] |= (bytes_a[i] & 0x01) << (15 - i);
-        app_regs.REG_CAPACITANCE_VALUES[50] |= (bytes_a[i] & 0x02) << (14 - i);
-        app_regs.REG_CAPACITANCE_VALUES[52] |= (bytes_a[i] & 0x04) << (13 - i);
-        app_regs.REG_CAPACITANCE_VALUES[54] |= (bytes_a[i] & 0x08) << (12 - i);
+        app_regs.REG_CAPACITANCE_VALUES[48] |= (uint16_t)((bytes_a[i] >> 0) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[50] |= (uint16_t)((bytes_a[i] >> 1) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[52] |= (uint16_t)((bytes_a[i] >> 2) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[54] |= (uint16_t)((bytes_a[i] >> 3) & 0x01) << (15 - i);
     }
 
     // Second channel
@@ -469,10 +481,10 @@ void calculate_capacitance_7()
     uint8_t *bytes = &bytes_a[16];
     for (uint8_t i = 0; i < 16; i++)
 	{
-        reg[48] |= (bytes[i] & 0x01) << (15 - i);
-        reg[50] |= (bytes[i] & 0x02) << (14 - i);
-        reg[52] |= (bytes[i] & 0x04) << (13 - i);
-        reg[54] |= (bytes[i] & 0x08) << (12 - i);
+        reg[48] |= (uint16_t)((bytes[i] >> 0) & 0x01) << (15 - i);
+        reg[50] |= (uint16_t)((bytes[i] >> 1) & 0x01) << (15 - i);
+        reg[52] |= (uint16_t)((bytes[i] >> 2) & 0x01) << (15 - i);
+        reg[54] |= (uint16_t)((bytes[i] >> 3) & 0x01) << (15 - i);
     }
 }
 
@@ -484,10 +496,10 @@ void calculate_capacitance_8()
 
     for (uint8_t i = 0; i < 16; i++)
 	{
-        app_regs.REG_CAPACITANCE_VALUES[56] |= (bytes_b[i] & 0x1) << (15 - i);
-        app_regs.REG_CAPACITANCE_VALUES[58] |= (bytes_b[i] & 0x2) << (14 - i);
-        app_regs.REG_CAPACITANCE_VALUES[60] |= (bytes_b[i] & 0x4) << (13 - i);
-        app_regs.REG_CAPACITANCE_VALUES[62] |= (bytes_b[i] & 0x8) << (12 - i);
+        app_regs.REG_CAPACITANCE_VALUES[56] |= (uint16_t)((bytes_b[i] >> 4) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[58] |= (uint16_t)((bytes_b[i] >> 5) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[60] |= (uint16_t)((bytes_b[i] >> 6) & 0x01) << (15 - i);
+        app_regs.REG_CAPACITANCE_VALUES[62] |= (uint16_t)((bytes_b[i] >> 7) & 0x01) << (15 - i);
     }
 
     // Second channel
@@ -495,9 +507,9 @@ void calculate_capacitance_8()
     uint8_t *bytes = &bytes_b[16];
     for (uint8_t i = 0; i < 16; i++)
 	{
-        reg[56] |= (bytes[i] & 0x1) << (15 - i);
-        reg[58] |= (bytes[i] & 0x2) << (14 - i);
-        reg[60] |= (bytes[i] & 0x4) << (13 - i);
-        reg[62] |= (bytes[i] & 0x8) << (12 - i);
+        reg[56] |= (uint16_t)((bytes[i] >> 4) & 0x01) << (15 - i);
+        reg[58] |= (uint16_t)((bytes[i] >> 5) & 0x01) << (15 - i);
+        reg[60] |= (uint16_t)((bytes[i] >> 6) & 0x01) << (15 - i);
+        reg[62] |= (uint16_t)((bytes[i] >> 7) & 0x01) << (15 - i);
     }
 }
